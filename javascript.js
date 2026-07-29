12f5d14faa9be8bb14bf0e9e23c097af57086798
[Timibluestar] <[mukegodsgoodness00@gmail.com]>
2026-07-29 06:44:53 -0700

    commit changes to my index.html hamburger menu and footer quick links
    style and javascript

--- javascript.js
+++ javascript.js
@@ -46,6 +46,13 @@ window.addEventListener("scroll",()=>{
 
 });
 
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navbar.classList.toggle("active");
+});
 //================ ACTIVE MENU =================
 
 const sections = document.querySelectorAll("section");
