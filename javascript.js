841de626bcbaefedbd438ea661e3fb53deb4053f
[Timibluestar] <[mukegodsgoodness00@gmail.com]>
2026-07-30 09:40:15 -0700

    refactor navigation structure and styles for improved accessibility and consistency

--- javascript.js
+++ javascript.js
@@ -53,43 +53,6 @@ menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active");
     navbar.classList.toggle("active");
 });
-//================ ACTIVE MENU =================
-
-const sections = document.querySelectorAll("section");
-
-const navLinks = document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll",()=>{
-
-    let current = "";
-
-    sections.forEach(section=>{
-
-        const sectionTop = section.offsetTop - 150;
-
-        const sectionHeight = section.clientHeight;
-
-        if(window.scrollY >= sectionTop){
-
-            current = section.getAttribute("id");
-
-        }
-
-    });
-
-    navLinks.forEach(link=>{
-
-        link.classList.remove("active");
-
-        if(link.getAttribute("href") === "#" + current){
-
-            link.classList.add("active");
-
-        }
-
-    });
-
-});
 
 // ===============================
 // Testimonial Slider
